46afae81f20b08155119d44efab31c9b6c03bd8f
Joseph Terzi <terzitech@gmail.com>
2025-05-10 20:19:47 -0400

    feat: implement help command for CLI; refactor argument handling to display help message

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import displayHelp from '../src/commands/help.js';
 import init from '../src/commands/init.js';
 
 // Set the FHR_ROOT_DIR environment variable
@@ -9,25 +10,33 @@ const args = process.argv.slice(2);
 
 // If no arguments are provided, show usage
 if (args.length === 0) {
-  console.log('Usage: fhr <command> [options]');
-  console.log('Commands:');
-  console.log('  init "<file_name>"   Initialize a new file with the given name');
-  console.log('Options:');
-  console.log('  --help            Show this help message');
-  process.exit(1);
+  displayHelp();
+  process.exit(0);
 }
 
 // Check first arg for command
 const command = args[0];
 const options = args.slice(1);
 
-// Handle the init command
-if (command === 'init') {
-  const fileName = options[0];
-  if (fileName) {
-    await init(fileName);
+const commands = {
+  help: async () => await help(),
+  init: async (options) => {
+    const fileName = options[0];
+    if (fileName) {
+      await init(fileName);
+    } else {
+      console.error('Error: No file name provided for init command.');
+      process.exit(1);
+    }
+  },
+};
+
+(async () => {
+  if (commands[command]) {
+    await commands[command](options);
   } else {
-    console.error('Error: No file name provided for init command.');
+    console.error(`Unknown command: ${command}`);
+    displayHelp();
     process.exit(1);
   }
-}
+})();
