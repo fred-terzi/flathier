eaeb5a85a63f3c99a38aadc4a216710d1c2b2aef
Joseph Terzi <terzitech@gmail.com>
2025-05-15 14:18:01 -0400

    fix: remove unused HTTP import from dataHandler.js

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -2,7 +2,6 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import getCustomExt from './utils/getCustomExt.js'; // Helper to get customExt from customExtStore.json
-import { get } from 'http';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
